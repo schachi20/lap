6712196d4e67fc9904c82699b6dd6e44f8a8300e
schachi20 <129503080+schachi20@users.noreply.github.com>
2024-02-14 08:01:16 +0100

    sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,1 +1,18 @@
 free -m > test.txt && nmcli >> test.txt && lsblk >> test.txt
+# IP-Adresse abrufen und in die Datei schreiben
+ip addr show | grep 'inet ' | awk '{print $2}' > Linux_Sysinfos.txt
+
+# MAC-Adresse abrufen und in die Datei schreiben
+ip link show | awk '/ether/ {print $2}' >> Linux_Sysinfos.txt
+
+# Gateway-Adresse abrufen und in die Datei schreiben
+ip route | awk '/default/ {print $3}' >> Linux_Sysinfos.txt
+
+# Arbeitsspeicherinformationen abrufen und in die Datei schreiben
+free -h | grep 'Mem' | awk '{print "Arbeitsspeicher: " $2}' >> Linux_Sysinfos.txt
+
+# Festplattenspeicherinformationen abrufen und in die Datei schreiben
+df -h | grep '/dev/' | awk '{print "Festplattenspeicher: " $2}' >> Linux_Sysinfos.txt
+
+Ausgabe der Systeminformationen in eine Datei vereinfacht
+nmcli > Linux_Sysinfos.txt && free >> Linux_Sysinfos.txt && lsblk >> Linux_Sysinfos.txt
