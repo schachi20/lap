94b49239ad98c641f7e67a0d6c09003a11dba697
schachi20 <129503080+schachi20@users.noreply.github.com>
2024-02-28 07:48:34 +0100

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,14 +1,14 @@
 # IP-Adresse abrufen und in die Datei schreiben
-ip addr show | grep 'inet ' | awk '{print "IP-Adresse: " $2}' > Linux_Sysinfos.txt
+ip addr show | grep 'inet ' | awk '{print $2}' > Linux_Sysinfos.txt
 
 # MAC-Adresse abrufen und in die Datei schreiben
-ip link show | awk '/ether/ {print "MAC-Adresse: " $2}' >> Linux_Sysinfos.txt
+ip link show | awk '/ether/ {print $2}' >> Linux_Sysinfos.txt
 
 # Gateway-Adresse abrufen und in die Datei schreiben
-ip route | awk '/default/ {print "Gateway-Adresse: " $3}' >> Linux_Sysinfos.txt
+ip route | awk '/default/ {print $3}' >> Linux_Sysinfos.txt
 
 # Arbeitsspeicherinformationen abrufen und in die Datei schreiben
-free -h | grep 'Mem' | awk '{print "Arbeitsspeicher: " $2}' >> Linux_Sysinfos.txt
+free -h | grep 'Mem' | awk '{print $2}' >> Linux_Sysinfos.txt
 
 # Festplattenspeicherinformationen abrufen und in die Datei schreiben
 df -h | grep '/dev/' | awk '{print "Festplattenspeicher: " $2}' >> Linux_Sysinfos.txt
